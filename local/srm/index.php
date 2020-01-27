f02caa038762c9567e7feb3b873ff4c4c5a6ff88
omidzahed <omid98.ir@gmail.com>
2020-01-27 11:28:44 +0330

    update srm plugin  (page init)

--- local/srm/index.php
+++ local/srm/index.php
@@ -96,6 +96,20 @@ $renderer = $PAGE->get_renderer('core_user');
 echo $renderer->unified_filter($course, $context, $filtersapplied, $baseurl);
 
 $table = new \local_srm\index_table($courseid, $groupid, $lastaccess, $roleid, $enrolid, $status, $searchkeywords, false, false);
-$table->out(15, 1);
+$table->out(3, 1);
 
 echo $OUTPUT->footer();
+?>
+
+<script >
+window.onload=function () {
+    setTimeout(function () {
+        $(".page-link").click(function (e) {
+            e.preventDefault();
+            let url=$(this).attr("href");
+                $("form").attr("action",$(this).attr("href"));
+                $("form").submit();
+            });
+    },1)
+    }
+</script>
--- local/srm/version.php
+++ local/srm/version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_srm';
-$plugin->release = '1.0.0';
+$plugin->release = '1.0.1';
 $plugin->version = 2019120100;
 $plugin->requires = 2016051700;
 $plugin->maturity = MATURITY_STABLE;
